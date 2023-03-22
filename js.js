5fab4495bd3bb667ec6d5aff62c5575ff7d1c6c8
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-03-22 14:59:10 -0700

    adding ghosts and movement to the game

--- js.js
+++ js.js
@@ -12,7 +12,15 @@ var player = {
   pacmouth: 320,
   pacdir: 0,
   psize: 32,
+  speed: 5,
+
 };
+
+var enemy = {
+  x: 150,
+  y: 200,
+  speed: 5,
+}
 
 var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
@@ -23,6 +31,60 @@ mainImage.ready = false;
 mainImage.onload = checkReady;
 mainImage.src = "pac.png";
 
+var keyclick = {};
+document.addEventListener("keydown", function (event) {
+  keyclick[event.keyCode] = true;
+  move(keyclick);
+}, false);
+
+document.addEventListener("keyup", function (event) {
+  delete keyclick[event.keyCode];
+}, false);
+
+function move(keyclick){
+    if(37 in keyclick){
+      player.x -= player.speed;
+      player.pacdir = 64;
+    }
+    if(38 in keyclick){
+      player.y -= player.speed;
+      player.pacdir = 96;
+    }
+    if(39 in keyclick){
+      player.x += player.speed;
+      player.pacdir = 0;
+    }
+    if(40 in keyclick){
+      player.y += player.speed;
+      player.pacdir = 32;
+    }
+
+    if(player.x >= (canvas.width - 32)){
+      player.x = 0;
+    }
+
+    if(player.y >= (canvas.height - 32)){
+      player.y = 0;
+    }
+
+    if(player.x < 0){
+      player.x = (canvas.width - 32);
+    }
+
+    if(player.y < 0){
+      player.y = (canvas.height - 32);
+    }
+
+
+    if(player.pacmouth == 320){
+      player.pacmouth = 352;
+    } else {
+      player.pacmouth = 320;
+    }
+
+    render();
+}
+
 function checkReady() {
   this.ready = true;
   playgame();
@@ -30,11 +92,33 @@ function checkReady() {
 
 function playgame() {
   render();
+  requestAnimationFrame(playgame);
+
 }
 
 function render() {
   context.fillStyle = "#000222";
   context.fillRect(0, 0, canvas.width, canvas.height);
+
+  
+  context.font = "20px monospace";
+  context.fillStyle = "white";
+  context.fillText(`Pacman: ${score} vs Ghost: ${gscore}`, 10,20)
+
+  //Drawing the Ghost
+  context.drawImage(
+    mainImage,
+    0,
+    0,
+    32,
+    32,
+    enemy.x,
+    enemy.y,
+    32,
+    32
+  );
+
+  //Drawing the Pacman
   context.drawImage(
     mainImage,
     player.pacmouth,
@@ -46,9 +130,7 @@ function render() {
     player.psize,
     player.psize
   );
-  context.font = "20px monospace";
-  context.fillStyle = "white";
-  context.fillText(`Pacman: ${score} vs Ghost: ${gscore}`, 10,20)
+  
 }
 
 document.body.appendChild(canvas);
